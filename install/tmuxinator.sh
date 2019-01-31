dd3cf3825f67fd400d47f90dbb1e13a7e0164b97
bdronneau <basti1.dr@gmail.com>
2019-01-31 17:11:27 +0100

    feat: add tmuxinator autocomplete

--- install/tmuxinator.sh
+++ install/tmuxinator.sh
@@ -9,6 +9,11 @@ main() {
     if command -v gem > /dev/null 2>&1; then
         gem install --user-install tmuxinator
     fi
+
+    if [[ ! -f "${HOME}/opt/bash-completion.d/tmuxinator.bash" ]]; then
+        curl -o "${HOME}/opt/bash-completion.d/tmuxinator.bash" "https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.bash"
+    fi
+    
 }
 
 main
