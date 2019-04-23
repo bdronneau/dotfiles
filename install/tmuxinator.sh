f52cdc66a3360d83959bac8748aa1c16124ae61d
bdronneau <basti1.dr@gmail.com>
2019-04-23 10:14:16 +0200

    refactor(tmuxinator): lock to version 0.15.0 because os x is rubty 2.3

--- install/tmuxinator.sh
+++ install/tmuxinator.sh
@@ -7,7 +7,12 @@ readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 main() {
     if command -v gem > /dev/null 2>&1; then
-        gem install --user-install tmuxinator
+        return
+    fi
+
+    if command -v tmuxinator > /dev/null 2>&1; then
+        # Lock to 0.15.0 because OS X use ruby 2.3.X
+        gem install --user-install tmuxinator -v 0.15.0
     fi
 
     if [[ ! -f "${HOME}/opt/bash-completion.d/tmuxinator.bash" ]]; then
