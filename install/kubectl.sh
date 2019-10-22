37d0f66124d5b7710c5a346ddaa2c72a5ed65903
bdronneau <basti1.dr@gmail.com>
2019-10-22 09:40:03 +0200

    fixes(kubectl): typo in command name

--- install/kubectl.sh
+++ install/kubectl.sh
@@ -7,18 +7,22 @@ readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 main() {
   local KUBECTL_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
-  if [[ ! -f "${HOME}/opt/kubetcl/kubetcl_${KUBECTL_VERSION}" ]]; then
-    mkdir -p "${HOME}/opt/kubetcl"
+  if [[ ! -f "${HOME}/opt/kubectl/kubectl_${KUBECTL_VERSION}" ]]; then
+    mkdir -p "${HOME}/opt/kubectl"
     local OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 
-    curl -o "${HOME}/opt/kubetcl/kubetcl_${KUBECTL_VERSION}" "https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/${OS}/amd64/kubectl"
-    chmod u+x "${HOME}/opt/kubetcl/kubetcl_${KUBECTL_VERSION}"
-    
+    curl -o "${HOME}/opt/kubectl/kubectl_${KUBECTL_VERSION}" "https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/${OS}/amd64/kubectl"
+    chmod u+x "${HOME}/opt/kubectl/kubectl_${KUBECTL_VERSION}"
+
+    if [[ -f "${HOME}/opt/bin/terraform" ]]; then
+      rm -f "${HOME}/opt/bin/kubectl"
+    fi
+
     # Activate version
-    ln -Fs "${HOME}/opt/kubetcl/kubetcl_${KUBECTL_VERSION}" "${HOME}/opt/bin/kubetcl"
+    ln -Fs "${HOME}/opt/kubectl/kubectl_${KUBECTL_VERSION}" "${HOME}/opt/bin/kubectl"
 
     # Generate bash completion
-    "${HOME}/opt/bin/kubetcl" completion bash > "${HOME}/opt/bash-completion.d/kubetcl"
+    "${HOME}/opt/bin/kubectl" completion bash > "${HOME}/opt/bash-completion.d/kubectl"
   fi
 }
 
