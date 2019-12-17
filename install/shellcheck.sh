5fd69d1ea227a6596aedb4123611009ffce2fe85
bdronneau <basti1.dr@gmail.com>
2019-12-17 15:42:37 +0100

    feat(install): add shellcheck

---
+++ install/shellcheck.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o nounset -o pipefail -o errexit
+
+install() {
+  if command -v brew > /dev/null 2>&1; then
+    brew install shellcheck
+  elif command -v apt-get > /dev/null 2>&1; then
+    sudo apt-get install -y -qq shellcheck
+  elif command -v pacman > /dev/null 2>&1; then
+    sudo pacman -Sq --noconfirm --needed shellcheck
+  fi
+}
