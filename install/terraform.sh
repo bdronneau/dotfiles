2982ca0cf6748fcdf4bf3e974bbbf5bdfad626ca
bdronneau <basti1.dr@gmail.com>
2019-12-17 15:43:00 +0100

    chore(terraform): bump version

--- install/terraform.sh
+++ install/terraform.sh
@@ -6,7 +6,7 @@ set -o pipefail
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 main() {
-  local TERRAFORM_VERSION=0.12.16
+  local TERRAFORM_VERSION=0.12.18
   if [[ ! -f "${HOME}/opt/terraform/terraform_${TERRAFORM_VERSION}" ]]; then
     mkdir -p "${HOME}/opt/terraform"
     local OS=$(uname -s)
