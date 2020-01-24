35665bb5c64943785913ea1cd141c754cc10f540
bdronneau <basti1.dr@gmail.com>
2020-01-24 10:11:32 +0100

    feat(install): add delta install

--- install/git.sh
+++ install/git.sh
@@ -7,4 +7,18 @@ install() {
     local SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
     curl -q -sS -o "${SCRIPT_DIR}/../sources/git-prompt" "https://raw.githubusercontent.com/git/git/v$(git --version | awk '{print $3}')/contrib/completion/git-prompt.sh"
   fi
+
+  local DELTA_VERSION="0.0.15"
+  local DELTA_ARCHIVE="delta-${DELTA_VERSION}-x86_64"
+
+  if [[ ${OSTYPE} =~ ^darwin ]]; then
+    DELTA_ARCHIVE+="-apple-darwin"
+  else
+    DELTA_ARCHIVE+="-unknown-linux-musl"
+  fi
+
+  curl -q -sSL --max-time 30 -O "https://github.com/dandavison/delta/releases/download/${DELTA_VERSION}/${DELTA_ARCHIVE}.tar.gz"
+  tar xzf "${DELTA_ARCHIVE}.tar.gz"
+  mv "${DELTA_ARCHIVE}/delta" "${HOME}/opt/bin/delta"
+  rm -rf "${DELTA_ARCHIVE}.tar.gz" "${DELTA_ARCHIVE}"
 }
