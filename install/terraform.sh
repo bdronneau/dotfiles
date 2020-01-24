62b2706ddab05ceb3f93acc74914de1e9057db3a
bdronneau <basti1.dr@gmail.com>
2020-01-24 10:12:07 +0100

    feat(terraform): bump version

--- install/terraform.sh
+++ install/terraform.sh
@@ -7,7 +7,7 @@ clean() {
 }
 
 install() {
-  local TERRAFORM_VERSION=0.12.18
+  local TERRAFORM_VERSION=0.12.20
   if [[ ! -f "${HOME}/opt/terraform/terraform_${TERRAFORM_VERSION}" ]]; then
     mkdir -p "${HOME}/opt/terraform"
     local OS=$(uname -s)
