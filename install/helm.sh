f3ff653a3fe25c700a149c386164ba1a5b62ccb7
bdronneau <basti1.dr@gmail.com>
2020-10-30 19:33:25 +0100

    feat: add helm

---
+++ install/helm.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -o nounset -o pipefail -o errexit
+
+clean() {
+  rm -rf "${HOME}/opt/helm*"
+}
+
+install() {
+  local HELM_VERSION=v3.4.0
+  if [[ ! -f "${HOME}/opt/helm/helm_${HELM_VERSION}" ]]; then
+    mkdir -p "${HOME}/opt/helm"
+
+    local OS
+    OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+    curl -q -sSL --max-time 300 -O "https://get.helm.sh/helm-${HELM_VERSION}-${OS}-amd64.tar.gz"
+
+    # Temp directory for extraction
+    if [[ ! -d "helm-${HELM_VERSION}" ]]; then
+      mkdir "helm-${HELM_VERSION}"
+    fi
+
+    tar -C "helm-${HELM_VERSION}" -xf "helm-${HELM_VERSION}-${OS}-amd64.tar.gz"
+    rm "helm-${HELM_VERSION}-${OS}-amd64.tar.gz"
+    mv "helm-${HELM_VERSION}/${OS}-amd64/helm" "${HOME}/opt/helm/helm_${HELM_VERSION}"
+    rm -Rf "helm-${HELM_VERSION}"
+
+    if [[ -f "${HOME}/opt/bin/helm" ]]; then
+      rm -f "${HOME}/opt/bin/helm"
+    fi
+
+    # Activate version
+    ln -Fs "${HOME}/opt/helm/helm_${HELM_VERSION}" "${HOME}/opt/bin/helm"
+  fi
+}
