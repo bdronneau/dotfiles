540c8bdcebad37fd2d766ccea2b4e07cb78712b6
bdronneau <basti1.dr@gmail.com>
2020-10-31 16:10:57 +0100

    refactor(software): add extensions

--- install/softwares.sh
+++ install/softwares.sh
@@ -8,4 +8,13 @@ install() {
   elif command -v pacman > /dev/null 2>&1; then
     sudo pacman -S --noconfirm --needed firefox code
   fi
+
+  if command -v code > /dev/null 2>&1; then
+    code --install-extension bungcip.better-toml
+    code --install-extension EditorConfig.EditorConfig
+    code --install-extension esbenp.prettier-vscode
+    code --install-extension golang.go
+    code --install-extension hashicorp.terraform
+    code --install-extension wholroyd.jinja
+  fi
 }
