cd79585a492171dbb431cdd5b70b340afaa72b67
bdronneau <basti1.dr@gmail.com>
2020-10-31 23:00:33 +0100

    feat(kubectl): add plugin to kubectl

--- install/kubectl.sh
+++ install/kubectl.sh
@@ -18,12 +18,15 @@ install() {
   local KUBECTL_VERSION
   KUBECTL_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
 
+  local KUBECTL_VIEW_SECRET_VERSION=0.6.0
+  local KUBECTL_KAIL_VERSION=0.15.0
+  local OS
+  OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+
   if [[ ! -f "${HOME}/opt/kubectl/kubectl_${KUBECTL_VERSION}" ]]; then
     mkdir -p "${HOME}/opt/kubectl"
     mkdir -p "${HOME}/opt/bash-completion.d"
 
-    local OS
-    OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 
     download "https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/${OS}/amd64/kubectl" "${HOME}/opt/kubectl/kubectl_${KUBECTL_VERSION}"
     chmod u+x "${HOME}/opt/kubectl/kubectl_${KUBECTL_VERSION}"
@@ -35,5 +38,29 @@ install() {
 
     # Generate bash completion
     "${KUBECTL_BIN}" completion bash > "${HOME}/opt/bash-completion.d/kubectl"
+  fi
+
+  if command -v kubectl > /dev/null 2>&1; then
+    if [[ ! -f "${HOME}/opt/kubectl/kubectl-view_secret-${KUBECTL_VIEW_SECRET_VERSION}" ]]; then
+      mkdir "${HOME}/opt/tmp/kubectl-view-secret_${KUBECTL_VIEW_SECRET_VERSION}"
+      download "https://github.com/elsesiy/kubectl-view-secret/releases/download/v${KUBECTL_VIEW_SECRET_VERSION}/kubectl-view-secret_${KUBECTL_VIEW_SECRET_VERSION}_${OS}_x86_64.tar.gz" "${HOME}/opt/tmp/kubectl-view-secret_${KUBECTL_VIEW_SECRET_VERSION}/kubectl-view-secret_${KUBECTL_VIEW_SECRET_VERSION}_x86_64.tar.gz"
+      pushd "${HOME}/opt/tmp/kubectl-view-secret_${KUBECTL_VIEW_SECRET_VERSION}"
+      tar xf "kubectl-view-secret_${KUBECTL_VIEW_SECRET_VERSION}_x86_64.tar.gz"
+      mv "kubectl-view-secret" "${HOME}/opt/kubectl/kubectl-view_secret-${KUBECTL_VIEW_SECRET_VERSION}"
+      ln -snF "${HOME}/opt/kubectl/kubectl-view_secret-${KUBECTL_VIEW_SECRET_VERSION}" "${HOME}/opt/bin/kubectl-view_secret"
+      popd
+      rm -Rf "${HOME}/opt/tmp/kubectl-view-secret_${KUBECTL_VIEW_SECRET_VERSION}"
+    fi
+
+    if [[ ! -f "${HOME}/opt/kubectl/kubectl-kail-${KUBECTL_KAIL_VERSION}" ]]; then
+      mkdir "${HOME}/opt/tmp/kubectl-kail_${KUBECTL_KAIL_VERSION}"
+      download "https://github.com/boz/kail/releases/download/v${KUBECTL_KAIL_VERSION}/kail_${KUBECTL_KAIL_VERSION}_${OS}_amd64.tar.gz" "${HOME}/opt/tmp/kubectl-kail_${KUBECTL_KAIL_VERSION}/kail.tar.gz"
+      pushd "${HOME}/opt/tmp/kubectl-kail_${KUBECTL_KAIL_VERSION}"
+      tar xf "kail.tar.gz"
+      mv "kail" "${HOME}/opt/kubectl/kubectl-kail-${KUBECTL_KAIL_VERSION}"
+      ln -snF "${HOME}/opt/kubectl/kubectl-kail-${KUBECTL_KAIL_VERSION}" "${HOME}/opt/bin/kubectl-kail"
+      popd
+      rm -Rf "${HOME}/opt/tmp/kubectl-kail_${KUBECTL_KAIL_VERSION}"
+    fi
   fi
 }
