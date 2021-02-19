afb4d9e0632f213c2d43d6b93756e7d06281b9df
bdronneau <basti1.dr@gmail.com>
2021-02-19 17:10:01 +0100

    refacotr(pip): delete python27

--- install/pip.sh
+++ install/pip.sh
@@ -3,11 +3,6 @@
 set -o nounset -o pipefail -o errexit
 
 install() {
-  if ! [ -f "${HOME}/Library/Python/2.7/bin/pip" ]; then
-    easy_install --user pip
-    export PATH="${HOME}/Library/Python/2.7/bin/:${PATH}"
-  fi
-
   if command -v pip3 > /dev/null 2>&1; then
     pip3 install --user virtualenvwrapper virtualenv==20.0.23
   fi
