c0fcf3652158057094ecdd3fb0ea53d5a02ceb8a
bdronneau <basti1.dr@gmail.com>
2021-05-25 16:26:48 +0200

    feat(pip): add upgrade

--- install/pip.sh
+++ install/pip.sh
@@ -4,6 +4,7 @@ set -o nounset -o pipefail -o errexit
 
 install() {
   if command -v pip3 > /dev/null 2>&1; then
+    pip3 install -U pip
     pip3 install --user virtualenvwrapper virtualenv==20.0.23
   fi
 }
