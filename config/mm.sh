4f8b1328ae9558cc81364eb5bf2aad3a73301fd3
bdronneau <basti1.dr@gmail.com>
2021-07-08 16:05:50 +0200

    feat(config): init new one

---
+++ config/mm.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+export DOTFILES__PACKAGES=true
+export DOTFILES__FIRST=true
+export DOTFILES_AWS=false
+export DOTFILES_CURLIE=true
+export DOTFILES_FZ=true
+export DOTFILES_FZF=true
+export DOTFILES_GIT=true
+export DOTFILES_GITOPS=true
+export DOTFILES_GOLANG=true
+export DOTFILES_GPG=true
+export DOTFILES_HELM=true
+export DOTFILES_JQ=true
+export DOTFILES_KUBECTL=true
+export DOTFILES_PIP=true
+export DOTFILES_SHELLCHECK=true
+export DOTFILES_SOFTWARES=true
+export DOTFILES_TERRAGRUNT=true
+export DOTFILES_TFENV=true
+export DOTFILES_TMUX=true
+export DOTFILES_YQ=true
