1a23615831dffc680abd59ea0f26223af020237e
bdronneau <basti1.dr@gmail.com>
2021-09-07 14:49:49 +0200

    chore(golang): clean deps

--- install/golang.sh
+++ install/golang.sh
@@ -52,6 +52,5 @@ install() {
     go get -u github.com/kisielk/errcheck
     go get -u golang.org/x/lint/golint
     go get -u golang.org/x/tools/cmd/goimports
-    go get -u github.com/davinche/gmux
   fi
 }
