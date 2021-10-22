8194b231052e80b564faf74894f3dc3efafd4d93
bdronneau <basti1.dr@gmail.com>
2021-10-22 14:32:46 +0200

    refactor(config): update

--- config/full.sh
+++ config/full.sh
@@ -2,7 +2,6 @@
 
 export DOTFILES__PACKAGES=true
 export DOTFILES__FIRST=true
-export DOTFILES_AWS=false
 export DOTFILES_CURLIE=true
 export DOTFILES_FZ=true
 export DOTFILES_FZF=true
@@ -19,9 +18,8 @@ export DOTFILES_PIP=true
 export DOTFILES_PYENV=true
 export DOTFILES_SHELLCHECK=true
 export DOTFILES_SOFTWARES=true
-export DOTFILES_SUBLIME=false
-export DOTFILES_TERRAFORM=false
-export DOTFILES_TERRAGRUNT=true
+export DOTFILES_TERRAGRUNT=false
+export DOTFILES_TGENV=true
 export DOTFILES_TFENV=true
 export DOTFILES_TMUX=true
 export DOTFILES_YQ=true
