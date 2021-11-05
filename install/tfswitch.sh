519bb50a1f130907bfa4b2cff1e02066f0364f4e
bdronneau <basti1.dr@gmail.com>
2021-11-05 10:45:43 +0100

    refactor(iac): replace tfenv and tgenv by tfswitch and tgswitch

---
+++ install/tfswitch.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -o nounset -o pipefail -o errexit
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../bin/utils.sh"
+
+TFSWITCH_BIN="${HOME}/opt/bin/tfswitch"
+
+clean() {
+  rm -rf "${TFSWITCH_BIN}"
+  rm -rf "${HOME}/opt/tfswitch"
+}
+
+install() {
+  local OS
+  OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+  local TFSWITCH_VERSION="0.12.1168"
+  if [[ ! -f "${HOME}/opt/tfswitch/tfswitch_${TFSWITCH_VERSION}" ]]; then
+    mkdir -p "${HOME}/opt/tfswitch"
+
+    url_tar "https://github.com/warrensbox/terraform-switcher/releases/download/${TFSWITCH_VERSION}/terraform-switcher_${TFSWITCH_VERSION}_${OS}_amd64.tar.gz" "tfswitch" "${HOME}/opt/tfswitch/tfswitch_${TFSWITCH_VERSION}"
+
+    if [[ -f "${TFSWITCH_BIN}" ]]; then
+      rm -f "${TFSWITCH_BIN}"
+    fi
+
+    # Activate version
+    ln -Fs "${HOME}/opt/tfswitch/tfswitch_${TFSWITCH_VERSION}" "${TFSWITCH_BIN}"
+  fi
+}
---
+++ install/tgswitch.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -o nounset -o pipefail -o errexit
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../bin/utils.sh"
+
+TGSWITCH_BIN="${HOME}/opt/bin/tgswitch"
+
+clean() {
+  rm -rf "${TGSWITCH_BIN}"
+  rm -rf "${HOME}/opt/tgswitch"
+}
+
+install() {
+  local OS
+  OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+  local TGSWITCH_VERSION="0.4.326"
+  if [[ ! -f "${HOME}/opt/tgswitch/tgswitch_${TGSWITCH_VERSION}" ]]; then
+    mkdir -p "${HOME}/opt/tgswitch"
+
+    url_tar "https://github.com/warrensbox/tgswitch/releases/download/${TGSWITCH_VERSION}/tgswitch_${TGSWITCH_VERSION}_${OS}_amd64.tar.gz" "tgswitch" "${HOME}/opt/tgswitch/tgswitch_${TGSWITCH_VERSION}"
+
+    if [[ -f "${TGSWITCH_BIN}" ]]; then
+      rm -f "${TGSWITCH_BIN}"
+    fi
+
+    # Activate version
+    ln -Fs "${HOME}/opt/tgswitch/tgswitch_${TGSWITCH_VERSION}" "${TGSWITCH_BIN}"
+  fi
+}
