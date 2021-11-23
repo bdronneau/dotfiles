4350086df905486c66ef768feb74e724673281f4
bdronneau <basti1.dr@gmail.com>
2021-11-23 11:43:07 +0100

    refactor(softwares): complete list

--- install/softwares.sh
+++ install/softwares.sh
@@ -6,7 +6,7 @@ install() {
   if command -v apt-get > /dev/null 2>&1; then
     sudo apt-get install -y -qq firefox
   elif command -v pacman > /dev/null 2>&1; then
-    sudo pacman -S --noconfirm --needed firefox code
+    sudo pacman -S --noconfirm --needed firefox code flatpak
   fi
 
   if command -v code > /dev/null 2>&1; then
@@ -16,5 +16,12 @@ install() {
     code --install-extension golang.go
     code --install-extension hashicorp.terraform
     code --install-extension wholroyd.jinja
+    code --install-extension waderyan.gitblame
+    code --install-extension 4ops.terraform
+  fi
+
+  if command -v flatpak > /dev/null 2>&1; then
+    flatpak install us.zoom.Zoom --or-update
+    flatpak install com.slack.Slack --or-update
   fi
 }
