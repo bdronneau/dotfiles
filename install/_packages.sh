54d8827e75bb84f4a35371ed0c4599a3bd514d7b
bdronneau <basti1.dr@gmail.com>
2022-04-06 23:20:50 +0200

    refactor(software): add more and more automation

--- install/_packages.sh
+++ install/_packages.sh
@@ -42,7 +42,7 @@ END_OF_BASH_PROFILE
 
     sudo apt-get update
     sudo apt-get upgrade -y -qq
-    sudo apt-get install -y -qq apt-transport-https
+    sudo apt-get install -y -qq apt-transport-https curl wget nmap netcat net-tools
 
     sudo apt-get install -y -qq "${PACKAGES[@]}" dnsutils
   elif command -v pacman > /dev/null 2>&1; then
--- install/softwares.sh
+++ install/softwares.sh
@@ -4,9 +4,17 @@ set -o nounset -o pipefail -o errexit
 
 install() {
   if command -v apt-get > /dev/null 2>&1; then
-    sudo apt-get install -y -qq firefox
+    sudo apt-get install wget gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+    rm -f packages.microsoft.gpg
+    sudo apt update
+    sudo apt-get install -y -qq firefox keepassxc code
   elif command -v pacman > /dev/null 2>&1; then
     sudo pacman -S --noconfirm --needed firefox code flatpak
+  elif command -v brew > /dev/null 2>&1; then
+    brew install keepassxc
   fi
 
   if command -v code > /dev/null 2>&1; then
