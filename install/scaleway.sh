69d966c9406c73d11b79f75b7761077ba156c4df
BastBotBuddy <56725623+3timeb@users.noreply.github.com>
2023-01-24 15:00:30 +0000

    chore(deps): update dependency scaleway/scaleway-cli to v2.10.0

--- install/scaleway.sh
+++ install/scaleway.sh
@@ -14,7 +14,7 @@ clean() {
 
 install() {
   # renovate: datasource=github-releases depName=scaleway/scaleway-cli
-  local SCW_VERSION="v2.8.0"
+  local SCW_VERSION="v2.10.0"
   if [[ ! -f "${HOME}/opt/scaleway/scaleway_${SCW_VERSION}" ]]; then
     mkdir -p "${HOME}/opt/scaleway"
     local OS
