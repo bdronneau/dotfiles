d7b16b803863c4fa29795672886c81ffadddd89c
BastBotBuddy <56725623+3timeb@users.noreply.github.com>
2023-03-10 07:00:22 +0000

    chore(deps): update dependency pyenv/pyenv to v2.3.15

--- install/pyenv.sh
+++ install/pyenv.sh
@@ -36,7 +36,7 @@ requirements() {
 
 install() {
   # renovate: datasource=github-tags depName=pyenv/pyenv
-  local PYENV_VERSION="v2.3.14"
+  local PYENV_VERSION="v2.3.15"
 
   if [[ ! -d "${HOME}/opt/pyenv/${PYENV_VERSION}" ]]; then
     requirements
