3056f6d700d7630553fb60db525b843fbc5ea2af
BastBotBuddy <basti1.dr+bastbot@gmail.com>
2023-05-11 16:00:28 +0200

    chore(deps): update dependency gruntwork-io/terragrunt to v0.45.11 (#225)

--- install/terragrunt.sh
+++ install/terragrunt.sh
@@ -11,7 +11,7 @@ clean() {
 
 install() {
   # renovate: datasource=github-tags depName=gruntwork-io/terragrunt
-  local TERRAGRUNT_VERSION="v0.45.10"
+  local TERRAGRUNT_VERSION="v0.45.11"
   if [[ ! -f "${HOME}/opt/terragrunt/terragrunt_${TERRAGRUNT_VERSION}" ]]; then
     mkdir -p "${HOME}/opt/terragrunt"
 
