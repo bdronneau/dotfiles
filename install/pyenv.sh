4eb09cd23d30375e1d0fe7a0b50356df31587946
BastBotBuddy <basti1.dr+bastbot@gmail.com>
2023-06-20 14:00:25 +0200

    chore(deps): update dependency pyenv/pyenv to v2.3.21 (#252)

--- install/pyenv.sh
+++ install/pyenv.sh
@@ -36,7 +36,7 @@ requirements() {
 
 install() {
   # renovate: datasource=github-tags depName=pyenv/pyenv
-  local PYENV_VERSION="v2.3.20"
+  local PYENV_VERSION="v2.3.21"
 
   if [[ ! -d "${HOME}/opt/pyenv/${PYENV_VERSION}" ]]; then
     requirements
