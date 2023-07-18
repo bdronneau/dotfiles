8dd5a1576140188d01a48a2888f21025871b827e
bdronneau <basti1.dr@gmail.com>
2023-07-18 07:17:16 +0200

    fix(colima): breaking path

--- install/colima.sh
+++ install/colima.sh
@@ -5,7 +5,7 @@ set -o nounset -o pipefail -o errexit
 install() {
   if command -v brew > /dev/null 2>&1; then
     brew install --quiet colima
-    colima nerdctl install --force --path ${HOME}/opt/bin/nerdctl
+    colima nerdctl install --force --path "${HOME}/opt/bin/nerdctl"
   else
     printf "Colima is not handle by this OS\n"
   fi
