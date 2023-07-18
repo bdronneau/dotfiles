612a65f98d50ca60de58d062f1044ab80fc99f4f
bdronneau <basti1.dr@gmail.com>
2023-07-18 08:59:16 +0200

    fix(jq): move to repository installation

--- install/jq.sh
+++ install/jq.sh
@@ -5,28 +5,10 @@ set -o nounset -o pipefail -o errexit
 cd "$(dirname "${BASH_SOURCE[0]}")" \
     && . "../bin/utils.sh"
 
-clean() {
-  rm -rf "${HOME}/opt/bin/jq*"
-  rm -rf "${HOME}/opt/jq*"
-}
-
 install() {
-  local JQ_VERSION=1.6
-  if [[ ! -f "${HOME}/opt/jq/jq_${JQ_VERSION}" ]]; then
-    mkdir -p "${HOME}/opt/jq"
-
-    local OS
-    OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-
-    download "https://github.com/stedolan/jq/releases/download/jq-${JQ_VERSION}/jq-${OS}64" "jq-${OS}64"
-    mv "jq-${OS}64" "${HOME}/opt/jq/jq_${JQ_VERSION}"
-
-    if [[ -f "${HOME}/opt/bin/jq" ]]; then
-      rm -f "${HOME}/opt/bin/jq"
-    fi
-
-    # Activate version
-    ln -Fs "${HOME}/opt/jq/jq_${JQ_VERSION}" "${HOME}/opt/bin/jq"
-    chmod u+x "${HOME}/opt/bin/jq"
+  if command -v apt-get > /dev/null 2>&1; then
+    sudo apt-get install -y -qq jq
+  elif command -v brew > /dev/null 2>&1; then
+    brew install --quiet jq
   fi
 }
