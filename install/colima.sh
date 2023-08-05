bc52162aefe1255330fb408bc50511c0420fb4f1
bdronneau <basti1.dr@gmail.com>
2023-08-05 15:30:46 +0200

    refactor(colima): add docker and buildx

--- install/colima.sh
+++ install/colima.sh
@@ -4,8 +4,10 @@ set -o nounset -o pipefail -o errexit
 
 install() {
   if command -v brew > /dev/null 2>&1; then
-    brew install --quiet colima
+    brew install --quiet colima docker docker-buildx
     colima nerdctl install --force --path "${HOME}/opt/bin/nerdctl"
+    mkdir -p ~/.docker/cli-plugins
+    ln -sfn /opt/homebrew/opt/docker-buildx/bin/docker-buildx ~/.docker/cli-plugins/docker-buildx
   else
     printf "Colima is not handle by this OS\n"
   fi
