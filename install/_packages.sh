1eadb2f2d83e95159329a2f2178a61b0db9e4495
bdronneau <basti1.dr@gmail.com>
2023-09-07 14:33:41 +0200

    feat(packages): add graphviz for diagrams

--- install/_packages.sh
+++ install/_packages.sh
@@ -30,7 +30,7 @@ END_OF_BASH_PROFILE
     brew install --quiet "${PACKAGES[@]}" bash-completion@2
     HOMEBREW_CASK_OPTS="" brew install --cask --quiet google-chrome spotify
     brew install --quiet --cask 1password 1password-cli nextcloud balenaetcher rectangle raycast firefox postico cyberduck  discord obs slack iterm2
-    brew install --quiet tailscale smug minio-mc
+    brew install --quiet tailscale smug minio-mc graphviz
 
     if [[ $(grep -c "$(brew --prefix)" "/etc/shells") -eq 0 ]]; then
       echo "+-------------------------+"
