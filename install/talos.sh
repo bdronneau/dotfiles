b2b1624579bea688f144c2dff98829b467383974
bdronneau <basti1.dr@gmail.com>
2023-09-15 14:27:01 +0200

    chore: bump version

--- install/talos.sh
+++ install/talos.sh
@@ -15,7 +15,7 @@ clean() {
 
 install() {
   # renovate: datasource=github-tags depName=siderolabs/talos
-  local TALOSCTL_VERSION=v1.4.5
+  local TALOSCTL_VERSION=v1.5.1
 
   local OS
   OS=$(uname -s | tr '[:upper:]' '[:lower:]')
