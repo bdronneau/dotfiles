86dabbcb3dedb0b017ab22f24ceb8606acd21e1f
BastBotBuddy <basti1.dr+bastbot@gmail.com>
2023-12-05 21:00:29 +0100

    chore(deps): update dependency cilium/cilium-cli to v0.15.17 (#334)

--- install/cilium.sh
+++ install/cilium.sh
@@ -16,7 +16,7 @@ clean() {
 
 install() {
   # renovate: datasource=github-tags depName=cilium/cilium-cli
-  local CILIUM_CLI_VERSION="v0.15.16"
+  local CILIUM_CLI_VERSION="v0.15.17"
   local CILIUM_CLI_BIN_VERSION_PATH="${HOME}/opt/cilium/cilium_${CILIUM_CLI_VERSION}"
   local CILIUM_CLI_BIN_NAME="cilium"
 
