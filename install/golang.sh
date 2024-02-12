ce6c25041315554245b622e987fc943b069a8277
bdronneau <basti1.dr@gmail.com>
2024-02-12 21:44:00 +0100

    feat(golang): add gofumpt

--- install/golang.sh
+++ install/golang.sh
@@ -56,5 +56,6 @@ install() {
     go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
     go install github.com/dkorunic/betteralign/cmd/betteralign@latest
     go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+    go install mvdan.cc/gofumpt@latest
   fi
 }
