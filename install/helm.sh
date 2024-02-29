3ec8a7c9bf8668a87879931832e85ef321b05da8
BastBotBuddy <56725623+3timeb@users.noreply.github.com>
2024-02-29 10:00:36 +0000

    chore(deps): update dependency helm/helm to v3.14.2

--- install/helm.sh
+++ install/helm.sh
@@ -11,7 +11,7 @@ clean() {
 
 install() {
   # renovate: datasource=github-tags depName=helm/helm
-  local HELM_VERSION="v3.14.0"
+  local HELM_VERSION="v3.14.2"
   # renovate: datasource=github-tags depName=hayorov/helm-gcs
   local HELM_PLUGIN_GCS_VERSION="0.4.3"
 
