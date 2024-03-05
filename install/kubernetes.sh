fd1922c7bfb04a5a263ea33a86170803a4edad22
BastBotBuddy <56725623+3timeb@users.noreply.github.com>
2024-03-05 20:00:30 +0000

    chore(deps): update dependency derailed/popeye to v0.20.5

--- install/kubernetes.sh
+++ install/kubernetes.sh
@@ -37,7 +37,7 @@ install() {
   local KUBE_SCORE_VERSION_TAG="v1.18.0"
   local KUBE_SCORE_VERSION="${KUBE_SCORE_VERSION_TAG/v/}"
   # renovate: datasource=github-releases depName=derailed/popeye
-  local POPEYE_VERSION_TAG="v0.20.4"
+  local POPEYE_VERSION_TAG="v0.20.5"
 
   local OS
   OS=$(uname -s | tr '[:upper:]' '[:lower:]')
