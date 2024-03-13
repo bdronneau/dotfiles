8dca8d65846e87320c5681c5b3d6fbf7f848a3da
BastBotBuddy <56725623+3timeb@users.noreply.github.com>
2024-03-13 16:00:18 +0000

    chore(deps): update dependency junegunn/fzf to v0.48.0

--- install/fzf.sh
+++ install/fzf.sh
@@ -12,7 +12,7 @@ clean() {
 
 install() {
   # renovate: datasource=github-tags depName=junegunn/fzf
-  local FZF_VERSION="0.47.0"
+  local FZF_VERSION="0.48.0"
   if [[ ! -f "${HOME}/opt/fzf/fzf_${FZF_VERSION}" ]]; then
     mkdir -p "${HOME}/opt/fzf"
 
