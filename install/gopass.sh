d0565cba23dc5db1203875a640f8a17d0f338a46
BastBotBuddy <basti1.dr+bastbot@gmail.com>
2024-03-17 14:00:20 +0100

    chore(deps): update dependency gopasspw/gopass to v1.15.12 (#393)

--- install/gopass.sh
+++ install/gopass.sh
@@ -14,7 +14,7 @@ clean() {
 
 install() {
   # renovate: datasource=github-tags depName=gopasspw/gopass
-  local GOPASS_VERSION="v1.15.11"
+  local GOPASS_VERSION="v1.15.12"
   if [[ ! -f "${HOME}/opt/gopass/gopass_${GOPASS_VERSION}" ]]; then
     mkdir -p "${HOME}/opt/gopass"
     local OS
