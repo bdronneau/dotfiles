5440f689b250f1a9ae01c7ed6d5907b9ceb93d33
BastBotBuddy <basti1.dr+bastbot@gmail.com>
2024-06-21 23:00:24 +0200

    chore(deps): update dependency siderolabs/talos to v1.7.5 (#460)

--- install/talos.sh
+++ install/talos.sh
@@ -15,7 +15,7 @@ clean() {
 
 install() {
   # renovate: datasource=github-tags depName=siderolabs/talos
-  local TALOSCTL_VERSION="v1.7.4" 
+  local TALOSCTL_VERSION="v1.7.5" 
 
   local OS
   OS=$(uname -s | tr '[:upper:]' '[:lower:]')
