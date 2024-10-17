aef0d4b5a06cdcb1cda5389de19570e1bbbf5b0a
BastBotBuddy <basti1.dr+bastbot@gmail.com>
2024-10-17 19:00:58 +0200

    chore(deps): update dependency pyenv/pyenv to v2.4.16 (#528)

--- install/pyenv.sh
+++ install/pyenv.sh
@@ -36,7 +36,7 @@ requirements() {
 
 install() {
   # renovate: datasource=github-tags depName=pyenv/pyenv
-  local PYENV_VERSION="v2.4.15"
+  local PYENV_VERSION="v2.4.16"
 
   if [[ ! -d "${HOME}/opt/pyenv/${PYENV_VERSION}" ]]; then
     requirements
