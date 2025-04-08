5ecdd4a57495e26fc93d36121cf495b3cefdef01
bdronneau <basti1.dr@gmail.com>
2025-04-08 11:06:47 +0200

    chore: format

--- install/_macos.sh
+++ install/_macos.sh
@@ -6,6 +6,7 @@ install() {
   if ! [[ ${OSTYPE} =~ ^darwin ]]; then
     return
   fi
+
   defaults write com.apple.screencapture show-thumbnail -bool false
   defaults write NSGlobalDomain AppleShowAllExtensions -bool true
   defaults write com.apple.finder AppleShowAllFiles -bool true
