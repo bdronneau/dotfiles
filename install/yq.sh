3034e4a11503604ee08dbbd90a20feb159571e80
BastBotBuddy <56725623+3timeb@users.noreply.github.com>
2025-05-03 07:00:59 +0000

    chore(deps): update dependency mikefarah/yq to v4.45.2

--- install/yq.sh
+++ install/yq.sh
@@ -12,7 +12,7 @@ clean() {
 
 install() {
   # renovate: datasource=github-tags depName=mikefarah/yq
-  local YQ_VERSION="v4.45.1"
+  local YQ_VERSION="v4.45.2"
   if [[ ! -f "${HOME}/opt/yq/yq_${YQ_VERSION}" ]]; then
     mkdir -p "${HOME}/opt/yq"
 
