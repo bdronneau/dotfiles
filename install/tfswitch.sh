08f2bd3cfb54d3c900ea5ba2926f1aa7dc388613
BastBotBuddy <basti1.dr+bastbot@gmail.com>
2025-07-24 16:01:19 +0200

    chore(deps): update dependency warrensbox/terraform-switcher to v1.4.7 (#672)

--- install/tfswitch.sh
+++ install/tfswitch.sh
@@ -19,7 +19,7 @@ install() {
   ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
 
   # renovate: datasource=github-tags depName=warrensbox/terraform-switcher
-  local TFSWITCH_VERSION="v1.4.6"
+  local TFSWITCH_VERSION="v1.4.7"
   if [[ ! -f "${HOME}/opt/tfswitch/tfswitch_${TFSWITCH_VERSION}" ]]; then
     mkdir -p "${HOME}/opt/tfswitch"
 
