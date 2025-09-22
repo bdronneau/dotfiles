883ddfc5d2fbe192bec46f24bc3c82274441b8ab
BastBotBuddy <56725623+3timeb@users.noreply.github.com>
2025-09-22 09:01:06 +0000

    chore(deps): update dependency deviceinsight/kafkactl to v5.13.0

--- install/kafka.sh
+++ install/kafka.sh
@@ -18,7 +18,7 @@ clean() {
 
 install() {
   # renovate: datasource=github-tags depName=deviceinsight/kafkactl
-  local APP_VERSION_TAG="v5.12.1"
+  local APP_VERSION_TAG="v5.13.0"
   local APP_VERSION="${APP_VERSION_TAG/v/}"
   local APP_BIN_VERSION_PATH="${APP_BASE_PATH}/${APP_BIN_NAME}_${APP_VERSION}"
 
