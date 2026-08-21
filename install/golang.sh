d0e1ef1a4309eae7dd9bc9a1fd69300680cf2596
bdronneau <basti1.dr@gmail.com>
2026-08-21 15:13:21 +0200

    feat(golang): bump golangci-lint

--- install/golang.sh
+++ install/golang.sh
@@ -55,7 +55,7 @@ install() {
     go install golang.org/x/tools/cmd/goimports@latest
     go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
     go install github.com/dkorunic/betteralign/cmd/betteralign@latest
-    go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+    go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest
     go install mvdan.cc/gofumpt@latest
   fi
 }
